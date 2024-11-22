f2e7d279ec3f83eab63616e3b0fcb13453f3c267
nicolettecheng <nikocheng@gmail.com>
2024-11-22 11:26:13 -0500

    update button classes

--- src/components/FaceChecker/FactChecker.jsx
+++ src/components/FaceChecker/FactChecker.jsx
@@ -1,28 +1,8 @@
 import "./FactChecker.scss";
-import closeIcon from "/assets/closeicon.svg"
+import closeIcon from "/assets/closeicon.svg";
 
-function FactChecker({
-  isOpen,
-  onClose,
-}) {
+function FactChecker({ isOpen, onClose }) {
   if (!isOpen) return null;
-
-//   const handleDelete = async () => {
-//     try {
-//       const endpoint =
-//         itemType === "inventory"
-//           ? `${apiUrl}/inventories/${itemId}`
-//           : `${apiUrl}/warehouses/${itemId}`;
-//       const response = await axios.delete(endpoint);
-
-//       if (response.status === 204) {
-//         onSuccess(); // Callback to refresh list or navigate
-//         onClose(); // Close the modal
-//       }
-//     } catch (error) {
-//       console.error(`Error deleting ${itemType} item:`, error);
-//     }
-//   };
 
   return (
     <div className="modal">
@@ -35,23 +15,19 @@ function FactChecker({
             onClick={onClose}
           />
           <div className="modal__text-container">
-            <h1 className="modal__title">
-              Verify with SmartTrust
-            </h1>
+            <h1 className="modal__title">Verify with SmartTrust</h1>
             <p className="modal__description">
-              Note: The community has responded to this post indicating false information. Majority of users are
-              confirming this new design is not official, the original Dogecoin design is the only verified design.
-
-              Would you like to learn more?
+              <b>Note</b>: The community has responded to this post indicating
+              false information. Majority of users are confirming this new
+              design is not official, the original Dogecoin design is the only
+              verified design. Would you like to learn more?
             </p>
           </div>
           <div className="modal__buttons">
-            <button className="button button--secondary" onClick={onClose}>
+            <button className="button button--cancel" onClick={onClose}>
               Cancel
             </button>
-            <button className="button button--delete">
-              Learn More
-            </button>
+            <button className="button button--learn">Learn More</button>
           </div>
         </div>
       </div>
