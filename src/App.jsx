97f4c41dbd222bb75af2eec8befcc1ae91611d65
Jennifern6 <jennifer_n@live.ca>
2024-11-22 08:48:43 -0500

    added profile pic, post image, styling of post

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import imgLike from "/assets/1.png";
 import imgComment from "/assets/2.png";
 import imgSend from "/assets/3.png";
 import imgShare from "/assets/4.png";
-// import imgVerify from "/assets/5.png";
+import imgVerify from "/assets/verify.png";
+import imgProfile from "/assets/profile.png";
+import imgPost from "/assets/chihuacoin.png";
 import "./App.scss";
 
 function App() {
@@ -13,10 +15,11 @@ function App() {
         <div className="facebook-post__header">
           <div className="facebook-post__header-left">
             <img
-              src="profile-picture.jpg"
-              alt="Profile Picture"
+              src={imgProfile}
+              alt="Profile Pic"
               className="facebook-post__profile-picture"
             />
+
             <div className="facebook-post__info">
               <p className="facebook-post__author">
                 Dogecoin Enthusiasts Group
@@ -26,65 +29,71 @@ function App() {
           </div>
           <button className="facebook-post__menu-button">...</button>
         </div>
+
         <div className="facebook-post__content">
           <p className="facebook-post__text">
-            🚀 Big News for Dogecoin Fans! 🐕💰 We’re thrilled to unveil the
-            future of Dogecoin: a brand-new logo design featuring the internet’s
-            most confident underdog… 🐶 The Chihuahua! Why the change? The
-            Dogecoin community voted, and it’s official: The Chihuahua
-            represents resilience, attitude, and the ability to bark louder than
-            its market cap. The new design, lovingly nicknamed “Chihuacoin,”
-            will roll out next week. Some exciting features: 🐾 Taco Tuesday
-            Bonuses: Double your mining rewards every Tuesday. 🌮🐾 Small Dog,
-            Big Dreams: Inspired by Chihuahuas, we’re aiming for $1 again (yes,
-            really). 🐾 Tiny Paws NFT Series: Collect exclusive Chihuahua
-            pawprint NFTs. Comment below: Are you ready to ride this wave?
+            🚀 Big News for Dogecoin Fans! 🐕💰
+            <br /> <br />
+            We’re thrilled to unveil the future of Dogecoin: a brand-new logo
+            design featuring the internet’s most confident underdog… 🐶 The
+            Chihuahua!
+            <br /> <br />
+            Why the change? The Dogecoin community voted, and it’s official:
+            The Chihuahua represents resilience, attitude, and the ability to bark
+            louder than its market cap.
+            <br /> <br />
+            The new design, lovingly nicknamed “Chihuacoin,” will roll out next
+            week. Some exciting features:
+            <br /> <br />
+            <ul>
+              <li>🐾 Taco Tuesday Bonuses: Double your mining rewards every Tuesday. 🌮</li>
+              <li>🐾 Small Dog, Big Dreams: Inspired by Chihuahuas, we’re aiming for $1 again (yes, really).</li>
+              <li>🐾 Tiny Paws NFT Series: Collect exclusive Chihuahua pawprint NFTs.</li>
+            </ul>
+            <br /> <br />
+            Comment below: Are you ready to ride this wave?
+            <br /> <br />
             🌊🐕💎 #Chihuacoin #DogeRebrand #CryptoWithAttitude
           </p>
+
           <div className="facebook-post__images">
-            <img
-              src="image1.jpg"
-              alt="Image 1"
-              className="facebook-post__image"
-            />
-            <div className="facebook-post__image-grid">
-              <img
-                src="image2.jpg"
-                alt="Image 2"
-                className="facebook-post__image"
-              />
-              <img
-                src="image3.jpg"
-                alt="Image 3"
-                className="facebook-post__image"
-              />
-            </div>
+            <img src={imgPost} alt="Post image of Chihuacoin" className="facebook-post__image" />
           </div>
         </div>
 
         <div className="facebook-post__actions">
           <button className="facebook-post__action facebook-post__action--like">
-            <img src={imgLike} alt="Like" className="button-icon" />
+            <img src={imgLike} alt="Like button" className="button-icon" />
             Like
           </button>
           <button className="facebook-post__action facebook-post__action--comment">
-            <img src={imgComment} alt="Like" className="button-icon" />
+            <img src={imgComment} alt="Comment button" className="button-icon" />
             Comment
           </button>
           <button className="facebook-post__action facebook-post__action--send">
-            <img src={imgSend} alt="Like" className="button-icon" />
+            <img src={imgSend} alt="Send button" className="button-icon" />
             Send
           </button>
           <button className="facebook-post__action facebook-post__action--share">
-            <img src={imgShare} alt="Like" className="button-icon" />
+            <img src={imgShare} alt="Share button" className="button-icon" />
             Share
           </button>
-          {/* <button className="facebook-post__action facebook-post__action--verify">
-            <img src={imgVerify} alt="Like" className="button-icon" />
+          <button className="facebook-post__action facebook-post__action--verify">
+            <img src={imgVerify} alt="Verify button" className="button-icon" />
             Verify
-          </button> */}
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
 
-          {/* <button className="facebook-post__action facebook-post__action--like">
+export default App;
+
+
+
+{
+  /* <button className="facebook-post__action facebook-post__action--like">
             {" "}
             <img src={imgLike} alt="Like" className="button-icon" /> Like{" "}
           </button>
@@ -103,11 +112,5 @@ function App() {
           <button className="facebook-post__action facebook-post__action--verify">
             {" "}
             <img src="public/assets/4.png" alt="Verify" className="button-icon" /> Verify{" "}
-          </button> */}
-        </div>
-      </section>
-    </div>
-  );
+          </button> */
 }
-
-export default App;
